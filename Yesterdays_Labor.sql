7ee454953c422187bc3266bb50dde28fedfa88da
JoshZastrow <j.a.zastrow.jr@gmail.com>
2016-07-25 15:12:41 -0400

    Rename header names in SQL query

--- Yesterdays_Labor.sql
+++ Yesterdays_Labor.sql
@@ -1,9 +1,18 @@
---Query of yesertdays labor notes in Epicor ERP system. If Today is monday (assuming no labor on sunday) pulls from Friday
+--Query of yesterday's labor notes in Epicor ERP system. If Today is monday (assuming no labor on sunday) pulls from Friday
 
 
 IF EXISTS ((SELECT * FROM ERP.LaborDtl WHERE Labordtl.ClockinDate = dateADD(day, -1, Convert(DAte, getdate()))))
     BEGIN
-      SELECT DATENAME(weekday, L.CLOCKINDATE), j.PartNum, l.JobNum, l.OprSeq, L.OprSeq,L.LaborType, E.Name, L.DspClockInTime, L.DspClockOutTime, L.LaborQty, L.LaborNote
+      SELECT
+        DATENAME(weekday, L.CLOCKINDATE),
+        j.PartNum,
+        l.JobNum,
+        L.OprSeq,
+        L.LaborType as 'Setup/Prod',
+        E.Name, L.DspClockInTime AS 'Start',
+        L.DspClockOutTime AS 'End',
+        L.LaborQty AS 'Qty',
+        L.LaborNote
       FROM ERP.LaborDtl AS l
         INNER JOIN erp.EmpBasic AS e
           ON l.company = e.company AND
@@ -16,7 +25,16 @@ IF EXISTS ((SELECT * FROM ERP.LaborDtl WHERE Labordtl.ClockinDate = dateADD(day,
       END
 ELSE
     BEGIN
-      SELECT DATENAME(weekday, L.CLOCKINDATE),j.PartNum, l.JobNum, l.OprSeq, L.OprSeq,L.LaborType, E.Name, L.DspClockInTime, L.DspClockOutTime, L.LaborQty, L.LaborNote
+      SELECT
+        DATENAME(weekday, L.CLOCKINDATE),
+        j.PartNum,
+        l.JobNum,
+        l.OprSeq,
+        L.LaborType as 'Setup/Prod',
+        E.Name, L.DspClockInTime AS 'Start',
+        L.DspClockOutTime AS 'End',
+        L.LaborQty AS 'Qty',
+        L.LaborNote
         FROM ERP.LaborDtl AS l
         INNER JOIN erp.EmpBasic AS e
           ON l.company = e.company AND
